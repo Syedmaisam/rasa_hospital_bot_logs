4b31cfed42e9495ec9bcab659d18a22b1f9b7977
Syedmaisam <syedmaisam_97@yahoo.com>
2021-07-30 16:22:18 +0500

    fix pagination

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -3,12 +3,18 @@
 <head>
     <title>Keywords</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 </head>
 <body>
 
 <div class="container">
     @yield('content')
 </div>
-
+<script>
+    $(document).ready(function(){
+        $(".w-5").css("height"," 12px");
+    });
+    </script>
 </body>
 </html>
