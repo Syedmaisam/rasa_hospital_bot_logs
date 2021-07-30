e65816369253f41632b2c1cb09360115506718b1
Syedmaisam <syedmaisam_97@yahoo.com>
2021-07-30 13:26:43 +0500

    updated work

---
+++ app/Http/Controllers/KeywordController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\keywords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+use App\Models\keyword_logs;
+class KeywordController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = keywords::orderBy('count', 'desc')->paginate(5);
+
+        return view('products.index',compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+          //  'detail' => 'required',
+        ]);
+
+        keywords::create($request->all());
+
+        return redirect()->route('products.index')
+                        ->with('success','Product created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(keywords $product)
+    {
+        return view('products.show',compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(keywords $product)
+    {
+        return view('products.edit',compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, keywords $product)
+    {
+        $request->validate([
+            'name' => 'required',
+          //  'detail' => 'required',
+        ]);
+
+        $product->update($request->all());
+
+        return redirect()->route('keyword.index')
+                        ->with('success','keyword updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy( $product)
+    {
+        $product_name = keywords::where('id',$product);
+        // dd($product_name->get());
+        $product_name->delete();
+
+        return redirect()->route('keyword.index')
+                        ->with('success','keyword deleted successfully');
+    }
+
+
+
+    public function getData(Request $request){
+        $name = Str::lower($request->name);
+        $data = keywords::where('name',$name )->first();
+        if($data == null){
+            $keyword = keywords::create(['name' => $name]);
+
+
+        }
+        else{
+           $keyword= keywords::find($data->id);
+           $keyword->count = $data->count + 1;
+           $keyword->save();
+        //    $data->update(['count' => $data->count + 1]);
+        }
+        // dd($keyword);
+
+        $keyword_logs = keyword_logs::create(['keyword_id'=>$keyword->id]);
+        return response()->json([
+            'message' => 'success',
+            'status' => true
+        ]);
+    }
+}
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,60 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+
+                <h2 style="margin-top: 5%;margin-bottom: 5%">Bot keyword list</h2>
+            </div>
+            {{-- <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('keyword.create') }}"> Create New Product</a>
+            </div> --}}
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Search count</th>
+            <th>last search</th>
+            <th width="280px">Action</th>
+        </tr>
+
+        @foreach ($products as $product)
+        @php
+            $date = \Carbon\Carbon::parse($product->created_at);
+            $now = \Carbon\Carbon::now();
+        @endphp
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->count }} times</td>
+            <td>{{  $date->diffForHumans($now) }}</td>
+            <td>
+                <form action="{{ route('keyword.destroy',$product->id) }}" method="POST">
+
+                    <a class="btn btn-info" href="{{ route('keyword.show',$product->id) }}">Show</a>
+
+                    {{-- <a class="btn btn-primary" href="{{ route('keyword.edit',$product->id) }}">Edit</a> --}}
+
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {!! $products->links() !!}
+
+@endsection
